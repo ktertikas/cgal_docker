4318c34133b8dae469a8e92376e7c0de066b39fa
Konstantinos Tertikas <ktertikas@gmail.com>
2021-12-07 14:57:57 +0200

    Fixing proper docker image

--- scripts/step_into.sh
+++ scripts/step_into.sh
@@ -5,5 +5,5 @@ docker run -it -e DISPLAY=$DISPLAY \
     --device=/dev/dri:/dev/dri \
     --mount source=/home/konstantinos/code,target=/code,type=bind \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    cgal:latest
+    ktertikas/cgal:latest
 
